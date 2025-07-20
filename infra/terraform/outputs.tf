260885f0eb8c740546a7a2b8f4f80bfca3cf13aa
Vinnicius Toth <vinni.toth@gmail.com>
2025-07-20 20:56:03 -0300

    infra: Ajustando parametros terraform

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,2 +1,9 @@
-bucket_name = aws_s3_bucket.my_bucket.bucket
-bucket_arn = aws_s3_bucket.my_bucket.arn
+output "bucket_name" {
+  description = "Nome do bucket S3 criado"
+  value       = aws_s3_bucket.my_bucket.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN do bucket S3 criado"
+  value       = aws_s3_bucket.my_bucket.arn
+}
