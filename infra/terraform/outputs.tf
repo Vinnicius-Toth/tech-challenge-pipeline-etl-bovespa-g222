20405f46ef41eb51e27ac1ad265da2258e72998c
Vinnicius Toth <vinni.toth@gmail.com>
2025-07-20 22:53:24 -0300

    infra: Ajustando resource do bucket ingestao

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "bucket_ingestao_etl_name" {
-  description = "Nome do bucket S3 criado"
-  value       = aws_s3_bucket.my_bucket.bucket
+  value = aws_s3_bucket.bucket_ingestao_etl_name.bucket
 }
 
 output "bucket_arn" {
-  description = "ARN do bucket S3 criado"
-  value       = aws_s3_bucket.my_bucket.arn
+  value = aws_s3_bucket.bucket_ingestao_etl_name.arn
 }
