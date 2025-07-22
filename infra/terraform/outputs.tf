ca9f0a50f22f3ed8f39ad883840c500835667ad0
Vinnicius Toth <vinni.toth@gmail.com>
2025-07-21 21:32:48 -0300

    infra: Subindo outros componentes

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -9,7 +9,3 @@ output "bucket_arn" {
 output "lambda_function_name" {
   value = aws_lambda_function.process_s3_event.function_name
 }
-
-output "lambda_role_arn" {
-  value = aws_iam_role.lambda_exec_role.arn
-}
