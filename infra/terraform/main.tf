48bf8249f9cfce6c4eef9464739213c3e558c114
Vinnicius Toth <vinni.toth@gmail.com>
2025-07-21 21:38:01 -0300

    infra: Subindo outros componentes

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,23 @@
+# Bucket S3 for ETL Ingestion
+resource "aws_s3_bucket" "bucket_ingestao_etl" {
+  bucket = var.bucket_ingestao_etl_name
+  acl    = var.acl
+
+  versioning {
+    enabled = true
+  }
+
+# Bucket artifacts for Lambda functions
+resource "aws_s3_bucket" "lambda_artifacts" {
+  bucket = var.bucket_artifact_name
+
+  versioning {
+    enabled = true
+  }
+
+}
+
+
 # Configure the S3 bucket for storing Terraform state files
 terraform {
   backend "s3" {
