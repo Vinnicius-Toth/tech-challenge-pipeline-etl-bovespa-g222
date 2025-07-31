a2907f7bbe4492621db37e6d8d3546bcfd1eaa11
Vinnicius Toth <vinni.toth@gmail.com>
2025-07-30 23:25:13 -0300

    feat: Alter lambda function

--- services/lambda/index.py
+++ services/lambda/index.py
@@ -1,4 +1,9 @@
 import json
+import boto3
+
+glue = boto3.client('glue')
+
+glue_job_name = 'glue_job_etl_ingestion_refined'
 
 def handler(event, context):
     for record in event.get('Records', []):
@@ -6,9 +11,17 @@ def handler(event, context):
         bucket_name = s3_info['bucket']['name']
         object_key = s3_info['object']['key']
         
-        print(f"Novo arquivo recebido no bucket {bucket_name}: {object_key}")
+        print(f"New file received in bucket {bucket_name}: {object_key}")
+
+        response = glue.start_job_run(
+            JobName=glue_job_name,
+            Arguments={
+                '--object_key': object_key  
+            }
+        )
+        print(f"Glue Job starded: {response['JobRunId']}")
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Evento processado com sucesso!')
+        'body': json.dumps('Event processing was successful!')
     }
